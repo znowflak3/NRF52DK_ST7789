c9c46e3f6f4b91ac86e3710db1f6be35f969a9f7
znowflak3 <joakim.znoflinga@yahoo.com>
2020-10-01 20:46:09 +0200

    added ignore file

---
+++ pca10040/s132/armgcc/_build/nrf52832_xxaa/app_util_platform.c.d
@@ -0,0 +1,152 @@
+_build/nrf52832_xxaa/app_util_platform.c.o: \
+ ../../../../../../components/libraries/util/app_util_platform.c \
+ ../../../../../../components/libraries/util/app_util_platform.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdint.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/stdint.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/_default_types.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/features.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_newlib_version.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_intsup.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_nvic.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/stdio.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_ansi.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/newlib.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/config.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/ieeefp.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/cdefs.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stddef.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdarg.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/reent.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_ansi.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_types.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/_types.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/lock.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/types.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/types.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/stdio.h \
+ /home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdbool.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_error_weak.h
+
+../../../../../../components/libraries/util/app_util_platform.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdint.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/stdint.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/_default_types.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/features.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_newlib_version.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_intsup.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../modules/nrfx/mdk/nrf.h:
+
+../../../../../../modules/nrfx/mdk/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_version.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/mpu_armv7.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_bitfields.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_name_change.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_soc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_svc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error_soc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_nvic.h:
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/stdio.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_ansi.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/newlib.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/config.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/cdefs.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stddef.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdarg.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/reent.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_ansi.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_types.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/_types.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/lock.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/types.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/types.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/stdio.h:
+
+/home/znowbolic/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdbool.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
